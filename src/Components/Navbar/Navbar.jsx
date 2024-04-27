fdfd2061dc3b2c90134e6d760637c6a85ff2f1f2
magedkarm <maged.karm341@gmail.com>
2024-04-27 16:28:37 +0300

    add Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,51 @@
 import React from "react";
-import { Container, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return <>dsa</>;
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid mx-5">
+          <Link to="" className="navbar-brand">
+            Navbar
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="" className="nav-link active" aria-current="page">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="" className="nav-link">
+                  Features
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="" className="nav-link">
+                  Pricing
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="" className="nav-link disabled" aria-disabled="true">
+                  Disabled
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
