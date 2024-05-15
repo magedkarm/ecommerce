9dae84d82f85a7e41c29af70f6764858d6d744f6
magedkarm <105291799+magedkarm@users.noreply.github.com>
2024-05-16 02:38:43 +0300

    Update Orders.jsx

--- src/Components/Orders/Orders.jsx
+++ src/Components/Orders/Orders.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { jwtDecode } from "jwt-decode";
 import axios from "axios";
 export default function Orders() {
   const [getOrders, setGetOrders] = useState(null);
